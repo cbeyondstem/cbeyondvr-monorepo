571c8f73b75911dce2bcbd4d43fcb4ea3bbf006a
Armelle Laine <armelle@cbeyondvr.com>
2019-11-23 01:32:05 -0800

    remove the deprecated site-starter, mdx-kit is not the starter

--- packages/site-starter/src/assets/index.js
+++
@@ -1,7 +0,0 @@
-// assets
-export {
-  default as landingPageStyles
-} from "./jss/material-kit-react/views/landingPage";
-export {
-  default as headerLinksStyle
-} from "./jss/material-kit-react/components/headerLinksStyle";
--- packages/site-starter/src/pages/index.jsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import { LandingPage } from 'views/LandingPage'
-
-export default function Index() {
-  return <LandingPage />
-}
