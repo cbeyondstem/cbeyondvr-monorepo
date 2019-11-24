1a0696743aaf69cbd01cc2a809e122d1623d65a1
Armelle Laine <armelle@cbeyondvr.com>
2019-11-24 01:36:56 -0800

    fix SEO so that it uses SiteConfigConsumer and not a gatsby graphql query directly, hence allowing it to be shared among multiple websites

--- packages/mdx-kit/src/components/SEO/Seo.tsx
+++ packages/mdx-kit/src/components/SEO/Seo.tsx
@@ -1,87 +1,71 @@
 import * as React from 'react'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
 import { SEOProps } from '@cbeyond/ui-kit'
+import { SiteConfigConsumer, SiteConfigProviderProps } from '../SiteConfig'
 
-const detailsQuery = graphql`
-  query DefaultSEOQuery {
-    site {
-      siteMetadata {
-        title
-        description
-        author
-      }
-    }
-  }
-`
-
-export function SEO(props: SEOProps) {
+export const SEO = (props: SEOProps) => {
   const { description = '', lang = 'en', meta = [], keywords = [], title } = props
   return (
-    <StaticQuery
-      query={detailsQuery}
-      render={data => {
-        const metaDescription = description || data.site.siteMetadata.description
-        return (
-          <Helmet
-            htmlAttributes={{
-              lang
-            }}
-            title={title}
-            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-            meta={[
-              {
-                name: `description`,
-                content: metaDescription
-              },
-              {
-                property: `og:title`,
-                content: title
-              },
-              {
-                property: `og:description`,
-                content: metaDescription
-              },
-              {
-                property: `og:type`,
-                content: `website`
-              },
-              {
-                name: `twitter:card`,
-                content: `summary`
-              },
-              {
-                name: `twitter:creator`,
-                content: data.site.siteMetadata.author
-              },
-              {
-                name: `twitter:title`,
-                content: title
-              },
-              {
-                name: `twitter:description`,
-                content: metaDescription
-              }
-            ]
-              .concat(
-                keywords.length === 0
-                  ? []
-                  : {
-                    name: `keywords`,
-                    content: keywords.join(`, `)
-                  }
-              )
-              .concat(
-                meta.length === 0
-                  ? []
-                  : {
-                    name: `meta`,
-                    content: meta.join(`, `)
-                  }
-              )}
-          />
-        )
-      }}
-    />
+    <SiteConfigConsumer>
+      {(cfg: SiteConfigProviderProps) => (
+        <Helmet
+          htmlAttributes={{
+            lang
+          }}
+          title={title}
+          titleTemplate={`%s | ${cfg.title}`}
+          meta={[
+            {
+              name: `description`,
+              content: description || cfg.description
+            },
+            {
+              property: `og:title`,
+              content: title
+            },
+            {
+              property: `og:description`,
+              content: description || cfg.description
+            },
+            {
+              property: `og:type`,
+              content: `website`
+            },
+            {
+              name: `twitter:card`,
+              content: `summary`
+            },
+            {
+              name: `twitter:creator`,
+              content: cfg.contact
+            },
+            {
+              name: `twitter:title`,
+              content: title
+            },
+            {
+              name: `twitter:description`,
+              content: description || cfg.description
+            }
+          ]
+            .concat(
+              keywords.length === 0
+                ? []
+                : {
+                  name: `keywords`,
+                  content: keywords.join(`, `)
+                }
+            )
+            .concat(
+              meta.length === 0
+                ? []
+                : {
+                  name: `meta`,
+                  content: meta.join(`, `)
+                }
+            )}
+        />
+      )}
+    </SiteConfigConsumer>
   )
 }
