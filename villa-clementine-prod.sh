d163be85a9dc8a67f493377f7136315a640accf3
Armelle Laine <armelle@cbeyondvr.com>
2020-01-08 13:42:05 -0800

    update villa-clementine-prod.sh

--- villa-clementine-prod.sh
+++ villa-clementine-prod.sh
@@ -1,6 +1,6 @@
 git checkout netlify-villa-clementine
 git merge --no-commit --no-ff master
-git reset HEAD .travis.yml
+git checkout --ours -- .travis.yml
 git add --all
 git commit -m "Merge from master"
 git push
