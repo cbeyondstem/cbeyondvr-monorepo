ca2e3d91dd2121cec34bf00a82a06e73d00d4d8e
Armelle Laine <armelle@cbeyondvr.com>
2020-01-10 16:43:02 -0800

    villa-clementine: fix Home Icons for Edge, add new pix

--- packages/ui-kit/src/components/ui/ContactUs/ContactUs.tsx
+++ packages/ui-kit/src/components/ui/ContactUs/ContactUs.tsx
@@ -1,44 +1,22 @@
+import * as _ from 'lodash'
 import * as React from 'react'
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 
-import Button from '@material-ui/core/Button'
-import MailOutlineIcon from '@material-ui/icons/MailOutline'
+const ContactUsButton = React.lazy(() => import('./ContactUsButton'))
 
-const ContactMeHref = React.lazy(() => import('./ContactUsHref'))
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    button: {
-      margin: theme.spacing(1),
-    },
-  })
-)
+export interface ContactUsProps {
+  title: React.ReactNode | string
+}
 
 // Make user click a button to show email adderss via lazy loading
-export const ContactUs: React.FunctionComponent = props => {
-  const classes = useStyles(props)
+export const ContactUs: React.FunctionComponent<ContactUsProps> = props => {
+  const { title } = props
 
-  const [showingEmail, setShowingEmail] = React.useState(false)
-
-  const emailComp = showingEmail ? (
-    <ContactMeHref />
-  ) : (
-    <Button
-      variant="contained"
-      color="primary"
-      onClick={() => setShowingEmail(true)}
-      startIcon={<MailOutlineIcon />}
-      className={classes.button}
-    >
-      Click for contact info
-    </Button>
-  )
   const isSSR = typeof window === 'undefined'
   return (
     <>
       {!isSSR && (
         <React.Suspense fallback={<div>loading...</div>}>
-          <div>{emailComp}</div>
+          <ContactUsButton title={title} />
         </React.Suspense>
       )}
     </>
