aff4339e8b11557f6954a31554895d3084fc7ae2
Armelle Laine <laine@aptolsystems.com>
2020-04-12 16:55:10 -0700

    github CI: update ui-kit/onPostBuild to use the github CI env variable names

--- packages/ui-kit/gatsby-node-utils/onPostBuild.js
+++ packages/ui-kit/gatsby-node-utils/onPostBuild.js
@@ -1,21 +1,35 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require("fs-extra");
-const path = require("path");
+const fs = require('fs-extra')
+const path = require('path')
 
 exports.getOnPostBuild = dir => () => {
-  const prefix = process.env.DEPLOY_TO_PREFIX || "";
+  const prefix =
+    process.env.DEPLOY_TO_PREFIX || process.env.INPUT_DEPLOY_TO_PREFIX || ''
   if (process.env.DEPLOY_TO) {
     // used for zeit now deployment
     fs.renameSync(
-      path.join(dir, "public"),
+      path.join(dir, 'public'),
       path.join(dir, prefix + process.env.DEPLOY_TO)
-    );
+    )
+  } else if (process.env.INPUT_DEPLOY_TO) {
+    // used for zeit now deployment
+    fs.renameSync(
+      path.join(dir, 'public'),
+      path.join(dir, prefix + process.env.INPUT_DEPLOY_TO)
+    )
   } else if (process.env.DEPLOY_MOVE_TO) {
     // used for netlify deployment
     fs.moveSync(
-      path.join(dir, "public"),
+      path.join(dir, 'public'),
       path.join(dir, process.env.DEPLOY_MOVE_TO),
       { overwrite: true }
-    );
+    )
+  } else if (process.env.INPUT_DEPLOY_MOVE_TO) {
+    // used for netlify deployment
+    fs.moveSync(
+      path.join(dir, 'public'),
+      path.join(dir, process.env.INPUT_DEPLOY_MOVE_TO),
+      { overwrite: true }
+    )
   }
-};
+}
