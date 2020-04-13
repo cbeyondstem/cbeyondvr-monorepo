279d41ab1927decb2c5dbba5a376857758d0c044
Armelle Laine <laine@aptolsystems.com>
2020-04-12 22:29:04 -0700

    github CI: try production deploy. deploy-dev skip

--- .github/actions/lerna2netlify/entrypoint.sh
+++ .github/actions/lerna2netlify/entrypoint.sh
@@ -5,4 +5,4 @@ cd packages/ui-kit
 yarn build
 cd ../../$INPUT_PACKAGE_LOCATION
 yarn build
-netlify --prod --auth=$SECRET_NETLIFY_AUTH --site=$SECRET_NETLIFY_SITE_ID --dir=.netlify/build
+netlify deploy --prod --auth $SECRET_NETLIFY_AUTH --site $SECRET_NETLIFY_SITE_ID --dir public
