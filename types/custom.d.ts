b626832837688b994fed15b496110a6369ff1017
Armelle Laine <laine@aptolsystems.com>
2020-07-13 13:50:30 -0700

    d

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -6,3 +6,13 @@ declare module '*.css' {
   const content: string
   export default content
 }
+
+declare module '*.comp.svg' {
+  const content: any
+  export default content
+}
+
+declare module '*.jpg' {
+  const content: string
+  export default content
+}
--- villa-clementine-prod.sh
+++
@@ -1,7 +0,0 @@
-git checkout netlify-villa-clementine
-git merge --no-commit --no-ff master
-git checkout --ours -- .travis.yml
-git add --all
-git commit -m "Merge from master"
-git push
-git checkout master
