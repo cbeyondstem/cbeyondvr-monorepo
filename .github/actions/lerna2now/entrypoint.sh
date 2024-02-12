d4e4f889d2677239f5c19e28c2ca7268dcd47c33
Armelle Laine <armelle@artizy.io>
2024-02-11 22:04:20 -0800

    github actions: remove lerna2now

--- .github/actions/lerna2now/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-export NODE_OPTIONS=--openssl-legacy-provider
-yarn install
-cd packages/ui-kit
-yarn build
-cd ../../$INPUT_PACKAGE_LOCATION
-yarn build
-yarn run now ./$INPUT_DEPLOY_TO_PREFIX$INPUT_DEPLOY_TO --prod --token=$SECRET_NOW_TOKEN
