678fbc005e8e4a65bb9b7cf8a5003d6c29174416
Armelle Laine <armelle@artizy.io>
2024-02-11 19:38:13 -0800

    use corepack to install latest yarnpkg in the project

--- .github/actions/lerna2netlify/entrypoint.sh
+++ .github/actions/lerna2netlify/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -l
-
 export NODE_OPTIONS=--openssl-legacy-provider
+corepack enable && \
+corepack use yarn@* && \
+yarn add netlify-cli
 yarn install
 cd packages/ui-kit
 yarn build
 cd ../../$INPUT_PACKAGE_LOCATION
 yarn build
-netlify deploy --prod --auth $SECRET_NETLIFY_AUTH --site $SECRET_NETLIFY_SITE_ID --dir public
+yarn run netlify deploy --prod --auth $SECRET_NETLIFY_AUTH --site $SECRET_NETLIFY_SITE_ID --dir public
